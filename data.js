10f9db5428c5237a10bf81dc9529872f0e1e031e
JS Queen <90614620+javascript-queen@users.noreply.github.com>
2022-12-24 15:39:19 -0600

    feat: update img input

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 export const menuArray = [
   {
-    image: '/img/tea.svg',
+    image: 'img/tea.svg',
     name: 'Bubble Milk Tea',
     ingredients: ['milk tea powder', 'tapioca pearls'],
     price: '10',
     id: 0,
   },
   {
-    image: '/img/matcha.svg',
+    image: 'img/matcha.svg',
     name: 'Green Matcha',
     ingredients: ['matcha tea powder', 'tapioca pearls'],
     price: '13',
     id: 1,
   },
   {
-    image: '/img/chocolate.svg',
+    image: 'img/chocolate.svg',
     name: 'Cosy Cocoa',
     ingredients: ['cocoa powder', 'vanilla pudding jelly'],
     price: '12',
     id: 2,
   },
   {
-    image: '/img/yam.svg',
+    image: 'img/yam.svg',
     name: 'Yam Drink',
     ingredients: ['sweet potato powder', 'coconut jelly'],
     price: '12',
